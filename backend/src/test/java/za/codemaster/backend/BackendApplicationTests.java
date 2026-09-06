a49085ef6c233757def2d74f642c0b3466f25939
Nelly <nebiljhb025@student.wethinkcode.ac.za>
2026-09-06 03:53:06 +0200

    Basic structure

---
+++ backend/src/test/java/za/codemaster/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package za.codemaster.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
